b8b4869093d1df44b93ac29a17502a9618d5dac3
Administrator <gitlab_admin_982e14@example.com>
2026-04-20 18:39:50 +0900

    v0.2.0

--- bonus/apps/docker.sh
+++ bonus/apps/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PREFIX=registry2.k8s.icchon.jp/root/iot
-VERSION=v0.1.0
+VERSION=v0.2.0
 APP1=$PREFIX/vote-app1:$VERSION
 APP2=$PREFIX/vote-app2:$VERSION
 APP3=$PREFIX/vote-app3:$VERSION
